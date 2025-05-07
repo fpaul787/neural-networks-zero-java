f7f4b7cb9bf1bed8753fce6f2b13039400f526bd
Frantz Paul <fpaul787@gmail.com>
2025-05-07 00:33:15 -0400

    backward pass and output

--- src/DenseLayer.java
+++ src/DenseLayer.java
@@ -8,7 +8,11 @@ public class DenseLayer {
     private int numberOfNeurons;
     private double[][] weights; // Weights for the layer
     private double[] biases; // Biases for the layer
+    private double[][] inputs; // Inputs to the layer
     private double[][] output; // Output of the layer
+    private double[][] dWeights; // Gradient of weights
+    private double[] dBiases; // Gradient of biases
+    private double[][] dInputs; // Gradient of inputs
 
     /**
      * Constructor for the DenseLayer.
@@ -38,6 +42,8 @@ public class DenseLayer {
      */
     public void forward(double[][] input) {
 
+        this.inputs = input; // Store the inputs for backpropagation
+        
         // Calculate the output values from inputs, weights, and biases
         int batchSize = input.length;
         int numberOfNeurons = this.numberOfNeurons;
@@ -59,11 +65,76 @@ public class DenseLayer {
     }
 
     /**
+     * Backward pass through the layer.
+     * @param dValues Gradient of the loss with respect to the output of this layer (2D array).
+     */
+    public void backward(double[][] dValues) {
+        
+        // Gradient on parameters, dot product of inputs and gradient on output
+        this.dWeights = new double[inputSize][numberOfNeurons];
+        double[][] inputsT = transpose(this.inputs); // Transpose the inputs for matrix multiplication
+        this.dWeights = dot(inputsT, dValues); // Gradient of weights
+
+        // Gradient on biases, sum of gradient on output
+        this.dBiases = new double[dValues[0].length];
+        for (int i = 0; i < dValues.length; i++) {
+            for (int j = 0; j < dValues[0].length; j++) {
+                this.dBiases[i] += dValues[j][i]; // Sum of gradients on output
+            }
+        }
+
+        // Gradient on inputs
+        double[][] weightsT = transpose(this.weights); // Transpose the weights for matrix multiplication
+        this.dInputs = dot(dValues, weightsT); // Gradient of inputs
+        
+    }
+
+    /**
      * Get the output of the layer.
      * @return 2D array of outputs (batch size x number of neurons).
      */
     public double[][] getOutput() {
         return this.output; // Return the output of the layer
+    }
+
+    /**
+     * Get the weights of the layer.
+     * @return 2D array of weights (input size x number of neurons).
+     */
+    public double[][] getWeights() {
+        return this.weights; // Return the weights of the layer
+    }
+
+    /**
+     * Get the biases of the layer.
+     * @return 1D array of biases (number of neurons).
+     */
+    public double[] getBiases() {
+        return this.biases; // Return the biases of the layer
+    }
+
+    /**
+     * Get the gradients of the weights.
+     * @return 2D array of gradients of weights (input size x number of neurons).
+     */
+    public double[][] getDWeights() {
+        return this.dWeights; // Return the gradient of weights
+    }
+
+    /**
+     * Get the gradients of the biases.
+     * @return 1D array of gradients of biases (number of neurons).
+     */
+    public double[] getDBiases() {
+        return this.dBiases; // Return the gradient of biases
+    }
+
+    /**
+     * Get the gradients of the inputs.
+     * @return 2D array of gradients of inputs (batch size x input size).
+     */
+    public double[][] getDInputs() {
+        return this.dInputs; // Return the gradient of inputs
     }
 
     private double[][] initializeWeights(int rows, int cols) {
@@ -82,6 +153,50 @@ public class DenseLayer {
             biases[i] = 0; // initialization of biases
         }
         return biases;
+    }
+
+    /**
+     * Transpose a matrix.
+     * @param matrix 2D array to be transposed.
+     * @return Transposed matrix (2D array).
+     */
+    private double[][] transpose(double[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        double[][] transposed = new double[cols][rows];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transposed[j][i] = matrix[i][j]; // Transpose the matrix
+            }
+        }
+        return transposed;
+    }
+
+    /**
+     * Matrix multiplication (dot product) of two matrices.
+     * @param a 
+     * @param b 
+     * @return Result of the dot product of a and b.
+     */
+    private double[][] dot(double[][] a, double[][] b) {
+        int rowsA = a.length;
+        int colsA = a[0].length;
+        int rowsB = b.length;
+        int colsB = b[0].length;
+
+        if (colsA != rowsB) {
+            throw new IllegalArgumentException("Incompatible dimensions for matrix multiplication.");
+        }
+
+        double[][] result = new double[rowsA][colsB];
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                for (int k = 0; k < rowsB; k++) {
+                    result[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return result;
     }
 
 
