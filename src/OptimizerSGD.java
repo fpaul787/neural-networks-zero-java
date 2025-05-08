d37b1523766cfba65623d3a4a05a6a982bdcd2e4
Frantz Paul <fpaul787@gmail.com>
2025-05-08 09:38:59 -0400

    optimizer updates

--- src/OptimizerSGD.java
+++ src/OptimizerSGD.java
@@ -1,37 +1,64 @@
 
-
 public class OptimizerSGD {
-    
-    private double learningRate; 
 
+    private double learningRate;
+    private double momentum;
+
+    public OptimizerSGD(double learningRate, double momentum) {
+        this.learningRate = learningRate;
+        this.momentum = momentum;
+    }
 
     public OptimizerSGD(double learningRate) {
         this.learningRate = learningRate;
+        this.momentum = 0.0;
     }
 
-    public void update_params(DenseLayer layer){
-        double [][] weights = layer.getWeights();
-        double [] biases = layer.getBiases();
+    public OptimizerSGD() {
+        this.learningRate = 1.0;
+        this.momentum = 0.0;
+    }
 
-        double [][] dWeights = layer.getDWeights();
-        double [] dBiases = layer.getDBiases();
+    public void update_params(DenseLayer layer) {
+        double[][] weights = layer.getWeights();
+        double[] biases = layer.getBiases();
 
-        double [][] updatedWeights = new double[weights.length][weights[0].length];
-        double [] updatedBiases = new double[biases.length];
+        double[][] dWeights = layer.getDWeights();
+        double[] dBiases = layer.getDBiases();
 
-        // update weights
-        for (int i = 0; i < weights.length; i++) {
-            for (int j = 0; j < weights[i].length; j++) {
-                updatedWeights[i][j] -= this.learningRate * dWeights[i][j];
+        double[][] updatedWeights = new double[weights.length][weights[0].length];
+        double[] updatedBiases = new double[biases.length];
+
+        if (this.momentum > 0) {
+
+
+        } else {
+
+            // Compute weight updates
+            for (int i = 0; i < weights.length; i++) {
+                for (int j = 0; j < weights[i].length; j++) {
+                    updatedWeights[i][j] = -this.learningRate * dWeights[i][j];
+                }
             }
-        }
 
-        // update biases
-        for (int i = 0; i < biases.length; i++) {
-            updatedBiases[i] -= this.learningRate * dBiases[i];
-        }
+            // Compute bias updates
+            for (int i = 0; i < biases.length; i++) {
+                updatedBiases[i] = -this.learningRate * dBiases[i];
+            }
 
-        layer.setWeights(updatedWeights);
-        layer.setBiases(updatedBiases);
+            // Apply the updates to the weights and biases
+            for (int i = 0; i < weights.length; i++) {
+                for (int j = 0; j < weights[i].length; j++) {
+                    updatedWeights[i][j] += weights[i][j];
+                }
+            }
+
+            for (int i = 0; i < biases.length; i++) {
+                updatedBiases[i] += biases[i];
+            }
+
+            layer.setWeights(updatedWeights);
+            layer.setBiases(updatedBiases);
+        }
     }
 }
