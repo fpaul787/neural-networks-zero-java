69982e6de408683edf3b479d6b169f8fa0571cfd
Frantz Paul <fpaul787@gmail.com>
2025-05-09 21:29:34 -0400

    format and getter function

--- src/App.java
+++ src/App.java
@@ -2,13 +2,13 @@ import models.Data;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
         // Create dataset
         // 100 samples, 3 classes
-        Data dataset = SpiralDataGenerator.createDataset(100, 3); 
-        double[][] X = dataset.X;
-        int[] y = dataset.y;
-        
+        Data dataset = SpiralDataGenerator.createDataset(100, 3);
+        double[][] X = dataset.getX();
+        int[] y = dataset.getY();
+
         // Create a DenseLayer with 2 input features and 3 output neurons
         // First DenseLayer with 2 inputs and 3 neurons
         DenseLayer denseLayer1 = new DenseLayer(2, 3);
@@ -16,28 +16,29 @@ public class App {
         // ReLU activation layer
         ActivationReLU activation1 = new ActivationReLU();
 
-        // Second DenseLayer with 3 input features (from the previous layer) and 3 output neurons
+        // Second DenseLayer with 3 input features (from the previous layer) and 3
+        // output neurons
         DenseLayer denseLayer2 = new DenseLayer(3, 3);
 
         // Softmax classifier's combined loss and activation layer
         ActivationCategoricalCrossentropySoftmaxLoss lossActivation = new ActivationCategoricalCrossentropySoftmaxLoss();
 
         // Learning rate and momentum
-        OptimizerSGD optimizer = new OptimizerSGD(); 
+        OptimizerSGD optimizer = new OptimizerSGD();
 
         int epochs = 10001; // Number of epochs
         for (int epoch = 0; epoch < epochs; epoch++) {
-            
+
             // Perfomance of forward pass through the first layer
             // Forward pass through the first layer
-            denseLayer1.forward(X); 
+            denseLayer1.forward(X);
 
             // Forward pass through ReLU activation
             activation1.forward(denseLayer1.getOutput());
 
             // Perform a forward pass through the second layer
             // Forward pass through the second layer
-            denseLayer2.forward(activation1.getOutput()); 
+            denseLayer2.forward(activation1.getOutput());
 
             // Forward pass through the loss layer
             double loss = lossActivation.forward(denseLayer2.getOutput(), y);
@@ -48,7 +49,7 @@ public class App {
             // Calculate accuracy
             double accuracy = calculateAccuracy(predictions, y);
 
-            // Backward pass 
+            // Backward pass
             lossActivation.backward(lossActivation.getOutput(), y);
             denseLayer2.backward(lossActivation.getDInputs());
             activation1.backward(denseLayer2.getDInputs());
@@ -67,7 +68,9 @@ public class App {
 
     /**
      * Get the index of the maximum value in each row of a 2D array.
-     * This method iterates through each row of the 2D array and finds the index of the maximum value in that row.
+     * This method iterates through each row of the 2D array and finds the index of
+     * the maximum value in that row.
+     * 
      * @param array 2D array of doubles
      * @return int array containing the index of the maximum value in each row
      *         of the input 2D array.
@@ -90,9 +93,11 @@ public class App {
 
     /**
      * Calculate the accuracy of predictions against the true labels.
-     * This method compares the predicted labels with the true labels and calculates the accuracy.
+     * This method compares the predicted labels with the true labels and calculates
+     * the accuracy.
+     * 
      * @param predictions int array of predicted class labels
-     * @param y int array of true class labels
+     * @param y           int array of true class labels
      * @return double representing the accuracy of the predictions
      */
     private static double calculateAccuracy(int[] predictions, int[] y) {
--- src/models/Data.java
+++ src/models/Data.java
@@ -1,11 +1,32 @@
 package models;
 
+/**
+ * Data class to hold the dataset
+ */
 public class Data {
-    public double[][] X; // 2D points
-    public int[] y; // class labels
+    public double[][] X;
+    public int[] y;
 
     public Data(double[][] X, int[] y) {
         this.X = X;
         this.y = y;
+    }
+
+    /**
+     * Get X values
+     * 
+     * @return
+     */
+    public double[][] getX() {
+        return X;
+    }
+
+    /**
+     * Get y values
+     * 
+     * @return
+     */
+    public int[] getY() {
+        return y;
     }
 }
